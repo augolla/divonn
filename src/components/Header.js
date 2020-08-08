6824acf64da278a8606acd6b57c3263c1da0b2bc
augolla <forig254@gmail.com>
2020-08-08 17:01:29 +0300

    yyy

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,39 @@
 import React from 'react'
+import {BsGear} from 'react-icons/bs'
+import './Header.css'
 
 class Header extends React.Component {
     render(){
         return(
-            <div style={{height:"800px"}}>
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+            <div style={{height:"800px"}} id='Divonn/construction'>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <a class="navbar-brand" href="#">Divonn</a>
+                    <h4 className="navbar-brand" style={{ fontWeight: '800px', color:'#009387'}}>Divonn</h4>
 
-                    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-                        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Overview <span class="sr-only">(current)</span></a>
+                    {/* <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+                        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Overview <span className="sr-only">(current)</span></a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Enterprise <span class="sr-only">(current)</span></a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Enterprise <span className="sr-only">(current)</span></a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Entertainment <span class="sr-only">(current)</span></a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Entertainment <span className="sr-only">(current)</span></a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Resources <span class="sr-only">(current)</span></a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Resources <span className="sr-only">(current)</span></a>
                             </li>
                         </ul>
                         
-                    </div>
+                    </div> */}
                 </nav>
+                <div className='gear'><BsGear size='5rem' color='#F84B81'/></div>
+                <h1 style={{ color:'#009387',fontWeight:'700'}}>Under Construction</h1>
+                <h4>Our site is currently undergoing maintenance.Sorry for the inconvinience</h4>
+                <h4>We will be back shortly.</h4>
             </div>
         )
     }
